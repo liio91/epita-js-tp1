a5cf4211232184be9cf92c7f8293909e21c63a85
Lionel LACOUTURE <lionel.lacouture@epita.fr>
2019-05-02 16:29:44 +0200

    added src/fixtures.js

--- src/fixtures.js
+++ src/fixtures.js
@@ -1,5 +1,12 @@
 // FIXME: add some meaningful data
 
-const pictures = [];
+const pictures = [
+    'https://picsum.photos/id/237/200/300',
+    'https://picsum.photos/id/1003/200/300',
+    'https://picsum.photos/id/1020/200/300',
+    'https://picsum.photos/id/1025/200/300',
+    'https://picsum.photos/id/1074/200/300'];
+
+JSON.parse(JSON.stringify(pictures))
 
 export default pictures;
